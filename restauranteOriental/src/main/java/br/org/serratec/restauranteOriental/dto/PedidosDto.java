e5b6fd939cba43c9f3c95ccc32be60863ea097d1
MiguelMFR <guelferreirar@gmail.com>
2024-05-15 22:14:10 -0300

    ok

--- restauranteOriental/src/main/java/br/org/serratec/restauranteOriental/dto/PedidosDto.java
+++ restauranteOriental/src/main/java/br/org/serratec/restauranteOriental/dto/PedidosDto.java
@@ -4,10 +4,14 @@ import br.org.serratec.restauranteOriental.model.Pedido;
 import br.org.serratec.restauranteOriental.model.Prato;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 public record PedidosDto(
 		Long idPedido,
+		@NotBlank(message = "O nome deverá ser preenchido!")
 		String cliente,
+		@NotNull(message = "O gênero não pode ser nulo.")
 		Prato prato){
 		
 	public Pedido toEntity() {
